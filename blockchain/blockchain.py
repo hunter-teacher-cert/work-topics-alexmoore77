ee1e1f3a09316a1f7b4d6a5dea2098bc6741f990
Alexander Moore <77898412+alexmoore77@users.noreply.github.com>
2022-04-18 05:08:16 +0000

    updated blockchain assignment

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -12,6 +12,7 @@ from time import time
 # simulate a unique number for the proof of work 
 import random
 
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
@@ -23,32 +24,23 @@ class Blockchain(object):
 #append the newest block to the chain.
     def new_block(self, proof, previous_hash=None):
         block = {
-#index of block=length of chain plus one
             'index': len(self.chain) + 1,
             'timestamp': time(),
-#pending transactions from old carry over to new
             'transactions': self.pending_transactions,
-#proof of work?
             'proof': proof,
-#a hash is an altered string.  What is this hash for?  What does the code below do?
-          'previous_hash': previous_hash or self.hash(self.chain[-1]) if self.chain else None,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]) if self.chain else None,
                 }
-#pending_transactions = []?  Huh?
         self.pending_transactions = []
-#adding to a new chain?
         self.chain.append(block)
         return block
 
 #Search the blockchain for the most recent block.
-
-#The @property decorator adds getters and setters
-@property
-def last_block(self):
-#The -1 in Python seems to refer to the previous list element
+    @property
+    def last_block(self):
         return self.chain[-1]
 
 # Add a transaction with relevant info to the 'blockpool'
-def new_transaction(self, sender, recipient, amount):
+    def new_transaction(self, sender, recipient, amount):
         transaction = {
             'sender': sender,
             'recipient': recipient,
@@ -60,7 +52,7 @@ def new_transaction(self, sender, recipient, amount):
 # receive one block. Turn it into a string, turn that into Unicode (for hashing).
 #Hash with SHA256 encryption, then translate the Unicode into a hexidecimal string.
 
-def hash(self, block):
+    def hash(self, block):
         string_object = json.dumps(block, sort_keys=True)
         block_string = string_object.encode()
         raw_hash = hashlib.sha256(block_string)
@@ -80,16 +72,24 @@ proof3 = '0000' + str(random.randint(1,21000000))
 myBlockchain=Blockchain()
 
 # STEP 2 print the empty chain that has the genesis block with proof =100
-
 print(myBlockchain.chain)
 
 # STEP 3 initiate the first transation of 1BTC from ___ to ___ by calling the new_transaction() on the object
- 
-new_transaction(myBlockchain.chain, "Alex","Eric","15")
-# STEP 4 add the new block to the chain using the new_block() by passing a proof argument for each
+myBlockchain.new_transaction("Alex","Iulian",15)
 
+# STEP 4 add the new block to the chain using the new_block() by passing a proof argument for each
+myBlockchain.new_block(proof1)
 # STEP 5 Print the chain with the new changes
+print(myBlockchain.chain)
 
 # STEP 6 Repeat steps 3-5 to record two more transactions
+
+myBlockchain.new_transaction("Alex","Eric", 20)
+myBlockchain.new_block(proof2)
+print(myBlockchain.chain)
+
+myBlockchain.new_transaction("Alex","Peter", 25)
+myBlockchain.new_block(proof3)
+print(myBlockchain.chain)
 
 #Note:(optional) You may add any formating sybmols to show how the chain looks at each phase
